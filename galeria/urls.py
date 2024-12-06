f2e4e87fcf5f03b760fa2d25dcd86965ef3442a5
rodrigogus94 <rodrigogus@live.com>
2024-12-06 12:48:05 -0300

    add paginas de imagens

--- galeria/urls.py
+++ galeria/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from galeria.views import index, imagem
 
 urlpatterns = [
-    path('', index),
-    path('imagem/', imagem)
+    path('', index, name = 'index'),
+    path('imagem/', imagem, name = 'imagem' ),
 
 ]
